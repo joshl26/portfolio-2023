4ed314583e09ba70b4bae23b1a63755b1db277ce
Joshua Lehman <joshlehman.dev@gmail.com>
2023-08-20 20:52:00 -0400

    commit

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import WebGL from "three/addons/capabilities/WebGL.js";
+if (WebGL.isWebGLAvailable()) {
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  const warning = WebGL.getWebGLErrorMessage();
+  document.getElementById("root").appendChild(warning);
+}
