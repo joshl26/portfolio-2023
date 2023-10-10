bc32f2cbecfd8f2245ba3f4806ffe78ea98497cc
Joshua Lehman <joshlehman.dev@gmail.com>
2023-10-10 00:02:17 -0400

    style-updates

--- src/components/ResponsiveMenu.jsx
+++ src/components/ResponsiveMenu.jsx
@@ -5,7 +5,25 @@ const ResponsiveMenu = () => {
   return (
     <section className="responsive-menu-section">
       <div className="responsive-menu-container">
-        <h2 className="responsive-menu-header">ResponsiveMenu</h2>
+        <div className="spacer"></div>
+        <a href="/home">
+          <h3 className="responsive-menu-header">Home</h3>
+        </a>
+        <a href="/work">
+          <h3 className="responsive-menu-header">Work</h3>
+        </a>
+        <a href="/portfolio">
+          <h3 className="responsive-menu-header">Portfolio</h3>
+        </a>
+        <a href="/about">
+          <h3 className="responsive-menu-header">About</h3>
+        </a>
+        <a href="/services">
+          <h3 className="responsive-menu-header">Services</h3>
+        </a>
+        <a href="/contact">
+          <h3 className="responsive-menu-header">Contact</h3>
+        </a>
       </div>
     </section>
   );
